87f77f784ca7f1fa0c8484f080538e33d3f0d7d0
spacemeowx2 <spacemeowx2@gmail.com>
2021-12-26 00:49:43 +0800

    feat: default log level is trace

--- src/bin/wlan_play.rs
+++ src/bin/wlan_play.rs
@@ -1,12 +1,12 @@
 use anyhow::Result;
-use wlan_play::config::ClientOpt;
 use env_logger::Env;
-use wlan_play::client::main as client_main;
 use structopt::StructOpt;
+use wlan_play::client::main as client_main;
+use wlan_play::config::ClientOpt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("wlan_play=info")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("wlan_play=trace")).init();
     let opt = ClientOpt::from_args();
 
     client_main(opt).await
